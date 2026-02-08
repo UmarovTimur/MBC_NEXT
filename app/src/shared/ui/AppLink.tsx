1b9482a94fa62d1af37ddf7217cfa41cc473f249
umarovtimur <umarovtimur2005@gmail.com>
2026-02-09 01:52:18 +0500

    add variants in AppLinks

--- app/src/shared/ui/AppLink.tsx
+++ app/src/shared/ui/AppLink.tsx
@@ -1,21 +1,29 @@
 import { AnchorHTMLAttributes, ReactNode } from "react";
 import { cn } from "@/shared/lib/utils";
 import Link from "next/link";
+import { cva, VariantProps } from "class-variance-authority";
 
-interface AppLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+const linkVariants = cva("transition-color duration-200 ", {
+  variants: {
+    variant: {
+      defalut: "text-gray-600 dark:text-gray-500 hover:text-blue-500 dark:hover:text-blue-300",
+      contrast: "text-gray-900 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-400",
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+});
+interface AppLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement>, VariantProps<typeof linkVariants> {
   href: string;
   children: ReactNode;
 }
 
-export const AppLink = ({ href, children, className, ...props }: AppLinkProps) => {
+export const AppLink = ({ href, children, className, variant, ...props }: AppLinkProps) => {
   const isExsternal = /^https?:\/\//.test(href) || href.startsWith("//");
   const isSpecial = href.startsWith("mailto:") || href.startsWith("tel:") || href.startsWith("#");
 
-  className = cn(
-    "text-gray-600 dark:text-gray-500 ",
-    "transition-color hover:text-blue-500 dark:hover:text-blue-300 ",
-    className,
-  );
+  className = linkVariants({ variant, className });
 
   if (isExsternal) {
     return (
@@ -27,10 +35,10 @@ export const AppLink = ({ href, children, className, ...props }: AppLinkProps) =
 
   if (isSpecial) {
     return (
-      <a href={href} className={className} {...props} >
+      <a href={href} className={className} {...props}>
         {children}
       </a>
-    )
+    );
   }
 
   return (
