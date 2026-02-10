218b9358931cbd1471a6aafbda30f5bda6a35ec6
umarovtimur <umarovtimur2005@gmail.com>
2026-02-11 03:38:28 +0500

    init workflow

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
-  basePath: "/mcdonald",
+  basePath: process.env.NODE_ENV === "production" ? "/mcdonald" : "",
   images: {
     unoptimized: true, // для static export рекомендуется, чтобы не было ошибок
+    loader: "custom",
+    loaderFile: "./src/shared/ui/ImageLoader.tsx",
   },
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
