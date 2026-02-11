fa024f38605b9dabd38ba806003abd7a49c7f3dd
umarovtimur <umarovtimur2005@gmail.com>
2026-02-11 05:18:19 +0500

    add mount state in the ThemeSwitcher

--- app/src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ app/src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,9 +3,20 @@
 import { Theme, useTheme } from "@/app/providers/Theme";
 import { Button } from "@/shared/ui/button";
 import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Button variant="outline" size="icon" disabled />;
+  }
+
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme}>
       {theme == Theme.DARK ? <Sun /> : <Moon />}
